fac667733796355f7c2936a3618fd25943c8e222
Konstantine <vdovin.kr@gmail.com>
2020-09-25 11:25:23 +0300

    Fix Article Comp.: get post data from redux state

--- src/comps/article/article.tsx
+++ src/comps/article/article.tsx
@@ -3,35 +3,30 @@ import React, {
     useState,
     useEffect
 } from 'react'
-import {useParams} from 'react-router-dom'
+import {useStore} from 'react-redux'
+import {useParams, Redirect} from 'react-router-dom'
 import {Post} from '../../redux/types'
 import Loader from '../loader'
 
-const Atricle: FunctionComponent = () => {
-    const [article, setArticle] = useState<Post>()
+const Article: FunctionComponent = () => {
     const {postID: url} = useParams()
+    const {posts}: {posts: Post[]} = useStore().getState()
 
-    useEffect(() => {
-        (async function() {
-            const answer = await fetch(`http://wp.vdovin28.beget.tech/wp-json/wp/v2/posts?slug="${url}"`)
-            const result = await answer.json()
-            setArticle(prev => ({...result[0]}) )
-        }())
-    }, [])
+    var article = posts.filter(i => decodeURI(i.slug) === url)[0]
 
-    useEffect(() => console.log(article), [article])
+    if (!article) return <Redirect to="/404" />
 
-    return !article
+    return (posts.length == 0)
         ? <Loader />
         : (
             <div className="article">
-                <header className="article__header"></header>
-                <div className="artile__body">
-                <div className="article__title">{article.title.rendered}</div>
-                <div className="article__content" dangerouslySetInnerHTML={{__html: article.content.rendered }}></div>
-                </div>
+                    <header className="article__header"></header>
+                    <div className="artile__body">
+                        <div className="article__title">{article.title.rendered}</div>
+                        <div className="article__content" dangerouslySetInnerHTML={{__html: article.content.rendered }}></div>
+                    </div>
             </div>
         )
 }
 
-export default Atricle
+export default Article
